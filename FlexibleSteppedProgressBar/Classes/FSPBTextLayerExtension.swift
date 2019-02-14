1cc60029c8618a8701453297185445737ce53d74
Michael Drozdowski <mdrozdowski@nextfaze.com>
2019-02-14 17:45:19 +1030

    Add flags to disable swiftlint for CATExtLayerExtension file

--- FlexibleSteppedProgressBar/Classes/FSPBTextLayerExtension.swift
+++ FlexibleSteppedProgressBar/Classes/FSPBTextLayerExtension.swift
@@ -12,13 +12,14 @@ import UIKit
 extension CATextLayer {
     
     func sizeWidthToFit() {
+        // swiftlint:disable force_cast
         let fontName = CTFontCopyPostScriptName(self.font as! CTFont) as String
         
         let font = UIFont(name: fontName, size: self.fontSize)
         
         let attributes = NSDictionary(object: font!, forKey: NSAttributedString.Key.font as NSCopying)
         
-        let attString = NSAttributedString(string: self.string as! String, attributes: attributes as? [NSAttributedString.Key : AnyObject])
+        let attString = NSAttributedString(string: self.string as! String, attributes: attributes as? [NSAttributedString.Key: AnyObject])
         
         var ascent: CGFloat = 0, descent: CGFloat = 0, width: CGFloat = 0
         
@@ -28,6 +29,7 @@ extension CATextLayer {
 
         width = ceil(width)
         
-        self.bounds = CGRect(x: 0, y: 0, width: width, height: ceil(ascent+descent))
+        self.bounds = CGRect(x: 0, y: 0, width: width, height: ceil(ascent + descent))
+    // swiftlint:enable force_cast
     }
 }
